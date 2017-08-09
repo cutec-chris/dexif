6555e3310cbe6e98c90f265cb00e22b68443de19
Andreas <andi@andifriess.com>
2017-08-09 07:23:00 +0200

    -added: Tools dir for tools like exiftools   a small script to show the actual Tags found by exiftool, for better compare with dExif

---
+++ tests/testpictures/makeinfo.cmd
@@ -0,0 +1,5 @@
+..\..\tools\exiftool -a DUTPic01.jpeg >DUTPic01.txt
+..\..\tools\exiftool -a DUTPic02.jpeg >DUTPic02.txt
+..\..\tools\exiftool -a DUTPic03.jpeg >DUTPic03.txt
+..\..\tools\exiftool -a DUTPicSelfimage01.jpeg >DUTPicSelfimage01.txt
+pause
