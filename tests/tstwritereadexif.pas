608eac209c125bbe538c9953cae99d807a0e5612
andi <friess@gmx.at>
2017-09-02 18:04:34 +0200

    -added: scandatetime from Freepascal/SysUtils to make Tests for Delphi work.

--- tests/tstwritereadexif.pas
+++ tests/tstwritereadexif.pas
@@ -20,7 +20,7 @@ uses
   {$ifdef FPC}
      , Graphics, SysUtils, fpcunit, testutils, testregistry
   {$else}
-     , TestFrameWork
+     , DateUtils, TestFrameWork , sysutils
   {$endif}
      , dEXIF;
 
@@ -65,15 +65,20 @@ type
     procedure Test_GPSLongitude_DM;
   end;
 
+{$ifndef FPC}
+function scandatetime(const pattern:string;const s:string;const fmt:TFormatSettings;startpos:integer=1) : tdatetime;  overload;
+
+function scandatetime(const pattern:string;const s:string;startpos:integer=1) : tdatetime; overload;
+
+{$endif}
 implementation
 
 uses
-  DateUtils, Math, dGlobal, dUtils
+  Math, dGlobal, dUtils
 {$ifdef FPC}
-   , FileUtil
+   ,DateUtils, FileUtil
 {$else}
   , Winapi.Windows
-  , sysutils
   , jpeg
 {$endif}
   ;
@@ -139,7 +144,7 @@ begin
   // nothing to do here
   {$IFDEF ERASE_TESTIMAGE}
     if FileExists(FDestFileName) then
-      DeleteFile(FDestFileName);
+      sysutils.DeleteFile(FDestFileName);
   {$ENDIF}
 end;
 
@@ -204,9 +209,9 @@ var
     strValue := TestParams[ATestID].Value;
     // !!!!!  ADD NEW TESTS HERE !!!!!!
     case ATestID of
-      0: DUT.ExifObj.DatetimeOriginal := {$ifdef FPC}ScanDatetime(ISODateFormat, strValue){$else}StrToDateTime(strValue){$endif}; // af: Delphi ????
-      1: DUT.ExifObj.DatetimeDigitized := {$ifdef FPC}ScanDatetime(ISODateFormat, strValue){$else}StrToDateTime(strValue){$endif}; // af: Delphi ????
-      2: DUT.ExifObj.DatetimeModified := {$ifdef FPC}ScanDatetime(ISODateFormat, strValue){$else}StrToDateTime(strValue){$endif}; // af: Delphi ????
+      0: DUT.ExifObj.DatetimeOriginal := ScanDatetime(ISODateFormat, strValue);
+      1: DUT.ExifObj.DatetimeDigitized := ScanDatetime(ISODateFormat, strValue);
+      2: DUT.ExifObj.DatetimeModified := ScanDatetime(ISODateFormat, strValue);
       3: DUT.Comment := strValue;
       4: DUT.Comment := strValue;
       5: DUT.ExifObj.Artist := strValue;
@@ -434,6 +439,329 @@ begin
   GenericTest(17);
 end;
 
+{$ifndef FPC}
+//*****************************************************************
+// Copied from Freepascal - Start
+//*****************************************************************
+function scandatetime(const pattern:string;const s:string;const fmt:TFormatSettings;startpos:integer=1) : tdatetime;
+
+var len ,ind  : integer;
+    yy,mm,dd  : integer;
+    timeval   : TDateTime;
+    activequote: char;
+
+procedure raiseexception(const s:string);
+
+begin
+  raise EConvertError.Create(s);
+end;
+
+
+
+procedure intscandate(ptrn:pchar;plen:integer;poffs:integer);
+// poffs is the offset to
+
+var
+    pind : integer;
+
+function findimatch(const mnts:array of string;p:pchar):integer;
+var i : integer;
+begin
+  result:=-1;
+  i:=0;
+  while (i<=high(mnts)) and (result=-1) do
+    begin
+      if AnsiStrLIComp(p,@mnts[i][1],length(mnts[i]))=0 then
+        result:=i;
+      inc(i);
+    end;
+end;
+
+procedure arraymatcherror;
+const
+  SNoArrayMatch                 = 'Can''t match any allowed value at pattern position %d, string position %d.';
+
+begin
+  raiseexception(format(SNoArrayMatch,[pind+1,ind]))
+end;
+
+function findmatch(const mnts : array of string;const s:string):integer;
+
+begin
+  result:=findimatch(mnts,@s[ind]);
+  if result=-1 then
+    arraymatcherror
+  else
+    begin
+      inc(ind,length(mnts[result])+1);
+      inc(pind,length(mnts[result])+1);
+      inc(result); // was 0 based.
+    end;
+end;
+
+var
+    pivot,
+    i     : integer;
+
+function scanfixedint(maxv:integer):integer;
+const
+  SPatternCharMismatch          = 'Pattern mismatch char "%s" at position %d.';
+var c : char;
+    oi:integer;
+begin
+  result:=0;
+  oi:=ind;
+  c:=ptrn[pind];
+  while (pind<plen) and (ptrn[pind]=c) do inc(pind);
+  while (maxv>0) and (ind<=len) and (s[ind] IN ['0'..'9']) do
+    begin
+      result:=result*10+ord(s[ind])-48;
+      inc(ind);
+      dec(maxv);
+    end;
+  if oi=ind then
+    raiseexception(format(SPatternCharMismatch,[c,oi]));
+end;
+
+procedure matchchar(c:char);
+const
+  SNoCharMatch                  = 'Mismatch char "%s" <> "%s" at pattern position %d, string position %d.';
+begin
+  if (ind>len) or (s[ind]<>c) then
+    raiseexception(format(SNoCharMatch,[s[ind],c,pind+poffs+1,ind]));
+  inc(pind);
+  inc(ind);
+end;
+
+function scanpatlen:integer;
+var c : char;
+    lind : Integer;
+begin
+  result:=pind;
+  lind:=pind;
+  c:=ptrn[lind];
+
+  while (lind<=plen) and (ptrn[lind]=c) do
+      inc(lind);
+  result:=lind-result;
+end;
+
+procedure matchpattern(const lptr:string);
+
+var len:integer;
+begin
+  len:=length(lptr);
+  if len>0 then
+    intscandate(@lptr[1],len,pind+poffs);
+end;
+
+const
+  SHHMMError                    = 'mm in a sequence hh:mm is interpreted as minutes. No longer versions allowed! (Position : %d).' ;
+  SFullpattern                  = 'Couldn''t match entire pattern string. Input too short at pattern position %d.';
+
+const
+  AMPMformatting : array[0..2] of string =('am/pm','a/p','ampm');
+
+const whitespace  = [' ',#13,#10];
+      hrfactor    = 1/(24);
+      minfactor   = 1/(24*60);
+      secfactor   = 1/(24*60*60);
+      mssecfactor = 1/(24*60*60*1000);
+
+var lasttoken,lch : char;
+
+begin
+  pind:=0;     lasttoken:=' ';
+  while (ind<=len) and (pind<plen) do
+     begin
+       lch:=upcase(ptrn[pind]);
+       if activequote=#0 then
+          begin
+            if (lch='M') and (lasttoken='H') then
+              begin
+                i:=scanpatlen;
+                if i>2 then
+                  raiseexception(format(Shhmmerror,[poffs+pind+1]));
+                timeval:=timeval+scanfixedint(2)* minfactor;
+              end
+            else
+            case lch of
+               'H':  timeval:=timeval+scanfixedint(2)* hrfactor;
+               'D':  begin
+                       i:=scanpatlen;
+                       case i of
+                          1,2 : dd:=scanfixedint(2);
+                          3   : dd:=findmatch(fmt.shortDayNames,s);
+                          4   : dd:=findmatch(fmt.longDayNames,s);
+                          5   : matchpattern(fmt.shortdateformat);
+                          6   : matchpattern(fmt.longdateformat);
+                         end;
+                     end;
+               'N':  timeval:=timeval+scanfixedint(2)* minfactor;
+               'S':  timeval:=timeval+scanfixedint(2)* secfactor;
+               'Z':  timeval:=timeval+scanfixedint(3)* mssecfactor;
+               'Y':  begin
+                       i:=scanpatlen;
+                       yy:=scanfixedint(i);
+                       if i<=2 then
+                         begin
+                           pivot:=YearOf(now)-fmt.TwoDigitYearCenturyWindow;
+                           inc(yy, pivot div 100 * 100);
+                           if (fmt.TwoDigitYearCenturyWindow > 0) and (yy < pivot) then
+                              inc(yy, 100);
+                         end;
+                      end;
+               'M': begin
+                       i:=scanpatlen;
+                       case i of
+                          1,2: mm:=scanfixedint(2);
+                          3:   mm:=findmatch(fmt.ShortMonthNames,s);
+                          4:   mm:=findmatch(fmt.LongMonthNames,s);
+                          end;
+                    end;
+               'T' : begin
+                       i:=scanpatlen;
+                       case i of
+                       1: matchpattern(fmt.shortdateformat);
+                       2: matchpattern(fmt.longtimeformat);
+                       end;
+                     end;
+               'A' : begin
+                            i:=findimatch(AMPMformatting,@ptrn[pind]);
+                            case i of
+                              0: begin
+                                   i:=findimatch(['AM','PM'],@s[ind]);
+                                   case i of
+                                     0: ;
+                                     1: timeval:=timeval+12*hrfactor;
+                                   else
+                                     arraymatcherror
+                                     end;
+                                   inc(pind,length(AMPMformatting[0]));
+                                   inc(ind,2);
+                                 end;
+                              1: begin
+                                    case upcase(s[ind]) of
+                                     'A' : ;
+                                     'P' : timeval:=timeval+12*hrfactor;
+                                   else
+                                     arraymatcherror
+                                     end;
+                                   inc(pind,length(AMPMformatting[1]));
+                                   inc(ind);
+                                 end;
+                               2: begin
+                                    i:=findimatch([fmt.timeamstring,fmt.timepmstring],@s[ind]);
+                                    case i of
+                                     0: inc(ind,length(fmt.timeamstring));
+                                     1: begin
+                                          timeval:=timeval+12*hrfactor;
+                                          inc(ind,length(fmt.timepmstring));
+                                        end;
+                                   else
+                                     arraymatcherror
+                                     end;
+                                   inc(pind,length(AMPMformatting[2]));
+                                   inc(pind,2);
+                                   inc(ind,2);
+                                 end;
+                            else  // no AM/PM match. Assume 'a' is simply a char
+                                matchchar(ptrn[pind]);
+                             end;
+                         end;
+               '/' : matchchar(fmt.dateSeparator);
+               ':' : begin
+                       matchchar(fmt.TimeSeparator);
+                       lch:=lasttoken;
+                     end;
+               #39,'"' : begin
+                           activequote:=lch;
+                           inc(pind);
+                         end;
+               'C' : begin
+                       intscandate(@fmt.shortdateformat[1],length(fmt.ShortDateFormat),pind+poffs);
+                       intscandate(@fmt.longtimeformat[1],length(fmt.longtimeformat),pind+poffs);
+                       inc(pind);
+                     end;
+               '?' : begin
+                       inc(pind);
+                       inc(ind);
+                     end;
+               #9  : begin
+                       while (ind<=len) and (s[ind] in whitespace) do
+                         inc(ind);
+                       inc(pind);
+                     end;
+               else
+                 matchchar(ptrn[pind]);
+             end; {case}
+             lasttoken:=lch;
+            end
+          else
+            begin
+              if activequote=lch then
+                begin
+                  activequote:=#0;
+                  inc(pind);
+                end
+              else
+                matchchar(ptrn[pind]);
+            end;
+     end;
+   if (pind<plen) and (plen>0) and (ptrn[plen-1]<>#9) then  // allow omission of trailing whitespace
+     RaiseException(format(SFullpattern,[poffs+pind+1]));
+end;
+
+var plen:integer;
+
+begin
+  activequote:=#0;
+  yy:=0; mm:=0; dd:=0;
+  timeval:=0.0;
+  len:=length(s); ind:=startpos;
+  plen:=length(pattern);
+  intscandate(@pattern[1],plen,0);
+  result:=timeval;
+  if (yy>0) and (mm>0) and (dd>0) then
+     result:=result+encodedate(yy,mm,dd);
+end;
+
+var
+  DefaultFormatSettings : TFormatSettings = (
+    CurrencyString: '$';
+    CurrencyFormat: 1;
+    CurrencyDecimals: 2;
+    DateSeparator: '-';
+    TimeSeparator: ':';
+    ListSeparator: ',';
+    ShortDateFormat: 'd/m/y';
+    LongDateFormat: 'dd" "mmmm" "yyyy';
+    TimeAMString: 'AM';
+    TimePMString: 'PM';
+    ShortTimeFormat: 'hh:nn';
+    LongTimeFormat: 'hh:nn:ss';
+    ShortMonthNames: ('Jan','Feb','Mar','Apr','May','Jun',
+                      'Jul','Aug','Sep','Oct','Nov','Dec');
+    LongMonthNames: ('January','February','March','April','May','June',
+                     'July','August','September','October','November','December');
+    ShortDayNames: ('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
+    LongDayNames:  ('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday');
+
+    ThousandSeparator: ',';
+    DecimalSeparator: '.';
+    TwoDigitYearCenturyWindow: 50;
+    NegCurrFormat: 5;
+  );
+
+function scandatetime(const pattern:string;const s:string;startpos:integer=1) : tdatetime; overload;
+
+begin
+ result:=scandatetime(pattern,s,defaultformatsettings,startpos);
+end;
+//*****************************************************************
+// Copied from Freepascal - End
+//*****************************************************************
+{$endif}
 
 initialization
   {$ifndef FPC}TestFramework.{$endif}RegisterTest(TTstWriteReadFile_dEXIF{$ifndef FPC}.Suite{$endif});
